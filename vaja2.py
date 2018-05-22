a49981b3bd522542f90c885725a24f54ad7f1647
syter12 <39103863+syter12@users.noreply.github.com>
2018-05-22 18:59:05 +0200

    fixes

--- vaja2.py
+++ vaja2.py
@@ -5,19 +5,22 @@ def read(port,rate):
     ser = serial.Serial(port, rate, timeout=1)
     ser.close()
     ser.open()
-    ser.write("wifi_conf_get".encode("ascii"))
+    ser.write("status_get\r\n")
 
-    time.sleep(2)
     read_line = ser.readline()
     print(read_line.decode())
+    while("PV2_EPD_TEMP_SENSOR" not in read_line):
+        read_line = ser.readline()
+        if "PV2_EPD_TEMP_SENSOR" in read_line:
+            print(read_line.split(":")[1])
     ser.close()
 
 
 
 def main():
     #port = input("Select port")
-    #rate = input("select baud rate")
-    read("com3","9600")
+
+    read("com3","115200")
 
 if __name__ == '__main__':
     main()
