edbb5798324ecdf3578e742577aa92f7a9e68e88
syter12 <39103863+syter12@users.noreply.github.com>
2018-05-29 22:58:21 +0200

    s

--- vaja2.py
+++ vaja2.py
@@ -3,26 +3,23 @@ import datetime
 import time
 
 class Beri():
-    def read(port,rate):
+    def read(self, port, rate):
         ser = serial.Serial(port, rate, timeout=1)  #initiate serial port
         ser.open()
         read_line = ser.readline()    #read line from serial
         print(read_line.decode())
         while("PV2_EPD_TEMP_SENSOR" not in read_line):  #read serial port until wanted line
             read_line = ser.readline()
-
-
         temp = read_line.split(":")[1]
         print("Temperature at {1} was {0} degrees celsius".format(temp.strip(), datetime.datetime.today().isoformat()))
         ser.close()
-        return(temp.strip(),datetime.datetime.today().isoformat())  
-    def write(port,rate):
+        return(temp.strip(),datetime.datetime.today().isoformat())
+
+    def write(self, port, rate):
         ser = serial.Serial(port, rate, timeout=1)
         ser.close()
         ser.open()
         ser.write("status_get\r\n")    #get status packet
-
-
 
 
 def main():
@@ -34,29 +31,8 @@ def main():
     while True:
         temp, date = Beri.read(port,rate)
 
-    read_line = ser.readline()
-    print(read_line.decode())
-    while("PV2_EPD_TEMP_SENSOR" not in read_line):
-        read_line = ser.readline()
-        if "PV2_EPD_TEMP_SENSOR" in read_line:
-            temp = read_line.split(":")[1]
-            print("Temperature at {1} was {0} degrees celsius".format(temp.strip(), datetime.datetime.today().isoformat()))
-            return(temp.strip(),datetime.datetime.today().isoformat())
-    ser.close()
-
-
-
-
-def main():
-    #port = input("Select port")
-    zbirka = []
-    a = 0
-
-    while True:
-        temp, date = read("/dev/ttyUSB0","115200")
-
         time.sleep(5)
-        zbirka.append(temp +","+ date)
+        zbirka.append(temp + "," + date)
         a+=1
         if a > 10:
             break
